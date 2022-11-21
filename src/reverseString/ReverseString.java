20a1d9810ba006f2851acf34470549fa0da3bf3e
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-11-21 22:35:08 +0700

    reverse string

---
+++ src/reverseString/ReverseString.java
@@ -0,0 +1,23 @@
+package reverseString;
+
+import java.util.Stack;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "Yolo22082000";
+        // String builder:
+        StringBuilder temp = new StringBuilder(input);
+        System.out.println("String builder: " + temp.reverse());
+        // Stack:
+        StringBuilder reverse = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+        int stackSize = stack.size();
+        for (int i = 0; i < stackSize; i++) {
+            reverse.append(stack.pop());
+        }
+        System.out.println("Stack method :" + reverse);
+    }
+}
